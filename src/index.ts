5665b8cbab9d8b77ada36a676624ef0ec16aa706
Michael Irvine <michael.j.irvine@gmail.com>
2022-11-06 11:19:47 -0500

    cleans up threading

--- src/index.ts
+++ src/index.ts
@@ -17,28 +17,35 @@ const app = new App({
 
 app.event('app_mention', async ({ event, say }) => {
   const text = stripUser(event.text)
+  // Get query
   const sqlQuery = await nlpService.nlqToSQL({
     text,
     jobId: process.env.JOB_ID as string,
     serviceToken: process.env.SERVICE_TOKEN as string,
   })
-  await say(
-    `<@${event.user}> here is your query in SQL:\n\`\`\`${sqlQuery}\`\`\``
-  )
+  await say({
+    text: `<@${event.user}> here is your query in SQL:\n\`\`\`${sqlQuery}\`\`\``,
+    thread_ts: event.ts,
+  })
+
+  // Run query against data
   const sqlQueryResult = await dataService.runQuery(sqlQuery)
   const { csv, pretty } = await formatQueryResult(sqlQueryResult)
-  const replyWithMarkdown = await say(
-    `<@${event.user}> here is what I found:\n\`\`\`${pretty}\`\`\``
-  )
+  await say({
+    text: `<@${event.user}> here is what I found:\n\`\`\`${pretty}\`\`\``,
+    thread_ts: event.ts,
+  })
   await app.client.files.upload({
     content: csv,
-    filename: `delphi_result_${replyWithMarkdown.ts}.csv`,
+    filename: `delphi_result_${event.ts}.csv`,
     filetype: 'csv',
     channels: event.channel,
-    thread_ts: replyWithMarkdown.ts,
+    thread_ts: event.ts,
     initial_comment: `Open this file in Excel or Google Docs to explore the full result`,
   })
 })
+
+// Start server
 ;(async () => {
   await app.start(process.env.PORT || 3000)
   console.info('⚡️ Bolt app is running!')
